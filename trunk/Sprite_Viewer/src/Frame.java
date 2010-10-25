481ad86d389ced5eede5a07bd24604ac775c5712
ComS 309 <knexer@gmail.com>
2010-10-25 05:10:50 +0000

    added features to the sprite viewer
    -Connor Schenck

--- trunk/Sprite_Viewer/src/Frame.java
+++ trunk/Sprite_Viewer/src/Frame.java
@@ -1,10 +1,14 @@
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.Scanner;
 
 import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -12,11 +16,13 @@ import javax.swing.JTextField;
 
 
 
-public class Frame extends JPanel {
+public class Frame extends JPanel implements ItemListener{
 
 	private JLabel file;
 	private JTextField time;
 	private Sprite frame;
+	private JCheckBox checkBox;
+	private boolean checked = false;
 	
 	public Frame(String filePath)
 	{
@@ -28,8 +34,13 @@ public class Frame extends JPanel {
 		this.add(new JLabel(icon));
 		file = new JLabel(new File(filePath).getName());
 		this.add(file);
-		time = new JTextField("  200");
+		time = new JTextField("          200");
 		this.add(time);
+		checkBox = new JCheckBox();
+		checkBox.setSelected(false);
+		checkBox.addItemListener(this);
+		checkBox.setPreferredSize(new Dimension(50, 50));
+		this.add(checkBox);
 		
 	}
 	
@@ -43,6 +54,26 @@ public class Frame extends JPanel {
 		} catch(Exception e) {
 			return 200;
 		}
+	}
+	
+	public void setTime(String times) {
+		time.setText(times);
+	}
+	
+	public boolean getChecked()
+	{
+		return checked;
+	}
+	
+	public void setChecked(boolean value)
+	{
+		checked = value;
+		checkBox.setSelected(value);
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent arg0) {
+		checked = arg0.getStateChange() == ItemEvent.SELECTED;
 	}
 	
 }
--- trunk/Sprite_Viewer/src/Window.java
+++ trunk/Sprite_Viewer/src/Window.java
@@ -8,6 +8,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferStrategy;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -19,9 +20,17 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JSplitPane;
+
+import parser.ConfigFile;
+import parser.Parser;
+import parser.ParserKeys;
 
 
 
@@ -31,7 +40,9 @@ public class Window implements ActionListener {
 	private Canvas canvas;
 	private BufferStrategy strategy;
 	private JButton addFiles;
-	private JButton clearFiles;
+	private JMenuItem clearFiles;
+	private JMenuItem export;
+	private JMenuItem open;
 	private JPanel scrollPanel;
 	
 	private ArrayList<Frame> list = new ArrayList<Frame>();
@@ -53,21 +64,45 @@ public class Window implements ActionListener {
 		frame = new JFrame("VN Game");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		//set up the menu
+		clearFiles = new JMenuItem("Clear Files");
+		clearFiles.addActionListener(this);
+		export = new JMenuItem("Export");
+		export.addActionListener(this);
+		open = new JMenuItem("Open");
+		open.addActionListener(this);
+		JMenuBar menuBar = new JMenuBar();
+		JMenu file = new JMenu("File");
+		JMenu edit = new JMenu("Edit");
+		file.add(export);
+		file.add(open);
+		edit.add(clearFiles);
+		menuBar.add(file);
+		menuBar.add(edit);
+		frame.setJMenuBar(menuBar);
+		
+		
+		//set up the buttons
 		addFiles = new JButton("Add Files");
 		addFiles.addActionListener(this);
-		clearFiles = new JButton("Clear Files");
-		clearFiles.addActionListener(this);
+		
+		ArithmaticPanel ap = new ArithmaticPanel(this);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(addFiles);
+		buttonPanel.add(new JSeparator(JSeparator.VERTICAL));
+		buttonPanel.add(ap);
+		
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
-		panel.add(addFiles, BorderLayout.NORTH);
-		panel.add(clearFiles, BorderLayout.SOUTH);
+		panel.add(buttonPanel, BorderLayout.NORTH);
 		scrollPanel = new JPanel();
 		JScrollPane scrollPane = new JScrollPane(scrollPanel);
 		panel.add(scrollPane, BorderLayout.CENTER);
 		
 		JSplitPane pane = new JSplitPane();
-		pane.setRightComponent(panel);
-		pane.setLeftComponent(canvas);
+		pane.setLeftComponent(panel);
+		pane.setRightComponent(canvas);
 		pane.setDividerLocation(500);
 		
 		frame.getContentPane().add(pane, BorderLayout.CENTER);
@@ -173,12 +208,105 @@ public class Window implements ActionListener {
 				}
 	        }
 		}
-		if(arg0.getSource().equals(clearFiles))
+		else if(arg0.getSource().equals(clearFiles))
 		{
 			scrollPanel.removeAll();
             scrollPanel.setLayout(new BoxLayout(scrollPanel, BoxLayout.Y_AXIS));
             newList = new ArrayList<Frame>();
 		}
+		else if(arg0.getSource().equals(export))
+		{
+			JFileChooser fc = new JFileChooser();
+			fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			if(fc.showSaveDialog(frame) == JFileChooser.APPROVE_OPTION)
+			{
+				File dir = fc.getSelectedFile();
+				if(!dir.exists())
+					dir.mkdir();
+				else if(dir.isFile())
+				{
+					dir.delete();
+					dir.mkdir();
+				}
+				
+				String names = "icon = ";
+				String times = "displayTime = ";
+				
+				for(Frame f : list)
+				{
+					File to = new File(dir, new File(f.getFrame().toString()).getName());
+					try {
+						FileCopy.copy(f.getFrame().toString(), to.getAbsolutePath());
+					} catch (IOException e) {
+						e.printStackTrace();
+						return;
+					}
+					
+					names += new File(f.getFrame().toString()).getName() + ", ";
+					times += f.getTime() + ", ";
+				}
+				
+				names = names.substring(0, names.lastIndexOf(','));
+				times = times.substring(0, times.lastIndexOf(','));
+				
+				try {
+					BufferedWriter writer = new BufferedWriter(new FileWriter(new File(dir, dir.getName() + ".cfg")));
+					writer.write(names);
+					writer.newLine();
+					writer.write(times);
+					writer.flush();
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		else if(arg0.getSource().equals(open))
+		{
+			JFileChooser fc = new JFileChooser();
+			
+			try {
+				Scanner s = new Scanner(new File("lastDirectory.txt"));
+				fc = new JFileChooser(s.nextLine());
+			} catch (FileNotFoundException e) {	}
+			
+			fc.setMultiSelectionEnabled(false);
+			int returnVal = fc.showOpenDialog(frame);
+
+	        if (returnVal == JFileChooser.APPROVE_OPTION) {
+	        	scrollPanel.removeAll();
+	            scrollPanel.setLayout(new BoxLayout(scrollPanel, BoxLayout.Y_AXIS));
+	            newList = new ArrayList<Frame>();
+	            
+	            Parser parser = null;
+	            try {
+	            	parser = new Parser(new ConfigFile(fc.getSelectedFile().getAbsolutePath()));
+	            	String[] names = parser.getList(ParserKeys.icon);
+	            	String[] times = parser.getList(ParserKeys.displayTime);
+	            	
+	            	ArrayList<Frame> newList = new ArrayList<Frame>();
+		            for(int i = 0; i < names.length; i++)
+		            {
+		            	Frame f = new Frame(new File(fc.getSelectedFile().getParentFile(), names[i]).getAbsolutePath());
+		            	f.setTime(times[i]);
+		            	newList.add(f);
+		            	scrollPanel.add(f);
+		            }
+		            
+		            this.newList = newList;
+		            
+		            frame.pack();
+	            } catch(Exception e) {
+	            	e.printStackTrace();
+	            	return;
+	            }
+	        }
+		}
+	}
+	
+	public ArrayList<Frame> getFrames()
+	{
+		return list;
 	}
 
 }
