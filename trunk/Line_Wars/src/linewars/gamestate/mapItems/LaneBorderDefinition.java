1ce0a95b7adfde2238d1d9df0cf06a5013856c77
ComS 309 <knexer@gmail.com>
2010-10-25 21:10:59 +0000

    updated the laneBorderDefinition class to reflect changes to the circle class
    -Connor Schenck

--- trunk/Line_Wars/src/linewars/gamestate/mapItems/LaneBorderDefinition.java
+++ trunk/Line_Wars/src/linewars/gamestate/mapItems/LaneBorderDefinition.java
@@ -2,6 +2,7 @@ package linewars.gamestate.mapItems;
 
 import java.io.FileNotFoundException;
 
+import linewars.gamestate.Position;
 import linewars.gamestate.Transformation;
 import linewars.gamestate.mapItems.strategies.collision.CollidesWithAll;
 import linewars.gamestate.shapes.Circle;
@@ -11,8 +12,7 @@ public class LaneBorderDefinition extends MapItemDefinition {
 
 	public LaneBorderDefinition(double size) throws FileNotFoundException, InvalidConfigFileException {
 		super();
-		//TODO set the radius of the circle
-		body = new Circle();
+		body = new Circle(new Transformation(new Position(0, 0), 0), size);
 		
 		cStrat = new CollidesWithAll();
 	}
