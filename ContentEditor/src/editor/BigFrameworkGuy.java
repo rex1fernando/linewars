961c09b4b61f63c556fec929c0af7fecad2272a5
ComS 309 <knexer@gmail.com>
2010-11-07 00:30:40 +0000

    Added the master framework class.  BigFrameworkGuy.  This class is the master editor class that holds references to all of the individual editors and does cool stuff with them.
    -Titus

---
+++ ContentEditor/src/editor/BigFrameworkGuy.java
@@ -0,0 +1,52 @@
+package editor;
+
+import javax.swing.JPanel;
+
+import linewars.configfilehandler.ConfigData;
+import linewars.configfilehandler.ParserKeys;
+
+public class BigFrameworkGuy implements ConfigurationEditor
+{
+	@Override
+	public void setData(ConfigData cd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void forceSetData(ConfigData cd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ConfigData getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ParserKeys getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JPanel getPanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
