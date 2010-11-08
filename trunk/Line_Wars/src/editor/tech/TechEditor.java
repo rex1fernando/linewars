ac215a409aa4d3944094e77e8f1e73ce51c6468b
ComS 309 <knexer@gmail.com>
2010-11-08 22:57:57 +0000

    Top-level class in the editor.tech package completed.
    -Taylor

--- trunk/Line_Wars/src/editor/tech/TechEditor.java
+++ trunk/Line_Wars/src/editor/tech/TechEditor.java
@@ -1,5 +1,7 @@
 package editor.tech;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
@@ -15,15 +17,35 @@ import editor.ConfigurationEditor;
 public class TechEditor implements ConfigurationEditor {
 
 	private BigFrameworkGuy framework;
-	private JPanel topLevel;
-	private JPanel uriSelector;
-	private JPanel keySelector;
-	private JPanel functionPanel;
 	
-	private HashMap<String, HashMap<ParserKeys, Modifier>> modificationChain;
-	private String name;
-	private String tooltip;
-	private Function costFunction;
+	private JPanel topLevel;
+	private JPanel bottomLevel;
+	
+	private NTCFEditor ntcf;
+	private TechURISelector modifiedURIs;
+	
+	public TechEditor(){
+		//init main components
+		ntcf = new NTCFEditor();
+		modifiedURIs = new TechURISelector();
+		
+		//set up layout
+		topLevel = new JPanel();
+		topLevel.setLayout(new BorderLayout());
+		bottomLevel = new JPanel();
+		bottomLevel.setLayout(new GridLayout(1, 2));
+		
+		//populate topLevel
+		topLevel.add(ntcf.getPanel(), BorderLayout.PAGE_START);
+		topLevel.add(bottomLevel, BorderLayout.CENTER);
+		
+		//populate bottomLevel
+		bottomLevel.add(modifiedURIs.getPanel());
+		//TODO add a dummy panel here? do we need to?
+		
+		//TODO does this have to be called here?
+		reset();
+	}
 	
 	/**
 	 * Forces the editor to load the config data object even if it
@@ -34,8 +56,29 @@ public class TechEditor implements ConfigurationEditor {
 	 */
 	@Override
 	public void forceSetData(ConfigData cd) {
-		// TODO Auto-generated method stub
+		ntcf.forceSetData(cd);
+		modifiedURIs.forceSetData(cd);
+	}
 
+	/**
+	 * Resets the editor to an empty configuration
+	 */
+	@Override
+	public void reset() {
+		ntcf.reset();
+		modifiedURIs.reset();
+	}
+
+	/**
+	 * Attempts to load the config data object cd. Throws an exception
+	 * if the data is corrupted and cannot be loaded.
+	 * 
+	 * @param cd
+	 */
+	@Override
+	public void setData(ConfigData cd) {
+		ntcf.setData(cd);
+		modifiedURIs.setData(cd);
 	}
 
 	/**
@@ -44,25 +87,17 @@ public class TechEditor implements ConfigurationEditor {
 	 */
 	@Override
 	public ConfigData getData() {
+		ConfigData ntcfData = ntcf.getData();
+		ConfigData modifiedURIsData = modifiedURIs.getData();
 		ConfigData ret = new ConfigData();
-		ret.add(ParserKeys.name, name);
-		ret.add(ParserKeys.tooltip, tooltip);
-		ret.add(ParserKeys.costFunction, costFunction.toConfigData());
-		
-		for(Entry<String, HashMap<ParserKeys, Modifier>> URI : modificationChain.entrySet()){
-			ConfigData modURI = new ConfigData();
-			modURI.add(ParserKeys.URI, URI.getKey());
-			
-			for(Entry<ParserKeys, Modifier> mod : modificationChain.get(URI.getKey()).entrySet()){
-				ConfigData modKey = new ConfigData();
-				modKey.add(ParserKeys.key, mod.getKey().toString());
-				modKey.add(mod.getKey(), modificationChain.get(URI.getKey()).get(mod.getKey()).toConfigData());
-				modURI.add(ParserKeys.modifiedKey, modKey);
-			}
-			
-			ret.add(ParserKeys.modifiedURI, modURI);
+		for(ParserKeys toAdd : ntcfData.getDefinedKeys()){
+			ret.set(toAdd, ntcfData.getStringList(toAdd).toArray(new String[0]));
+			ret.set(toAdd, ntcfData.getConfigList(toAdd).toArray(new ConfigData[0]));
 		}
-		
+		for(ParserKeys toAdd : modifiedURIsData.getDefinedKeys()){
+			ret.set(toAdd, modifiedURIsData.getStringList(toAdd).toArray(new String[0]));
+			ret.set(toAdd, modifiedURIsData.getConfigList(toAdd).toArray(new ConfigData[0]));
+		}
 		return ret;
 	}
 
@@ -87,29 +122,7 @@ public class TechEditor implements ConfigurationEditor {
 	 */
 	@Override
 	public boolean isValid() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	/**
-	 * Resets the editor to an empty configuration
-	 */
-	@Override
-	public void reset() {
-		// TODO Auto-generated method stub
-
-	}
-
-	/**
-	 * Attempts to load the config data object cd. Throws an exception
-	 * if the data is corrupted and cannot be loaded.
-	 * 
-	 * @param cd
-	 */
-	@Override
-	public void setData(ConfigData cd) {
-		// TODO Auto-generated method stub
-
+		return ntcf.isValid() && modifiedURIs.isValid();
 	}
 
 }
