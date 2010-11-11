cc452cbf5fecb511857fae2965779f66aecc8d79
ComS 309 <knexer@gmail.com>
2010-11-11 02:14:57 +0000

    Updated the MultiplayerDriver class.
    -tiuts

--- trunk/Line_Wars/src/linewars/init/MultiPlayerDriver.java
+++ trunk/Line_Wars/src/linewars/init/MultiPlayerDriver.java
@@ -1,5 +1,25 @@
 package linewars.init;
 
-public class MultiPlayerDriver {
+import java.util.ArrayList;
 
+public class MultiPlayerDriver {
+	public static void main(String[] args) {
+		String mapDefinitionURI = "resources/maps/3_lane_map.cfg";
+		int numPlayers = 2;
+		String serverAddress = "127.0.0.1";
+		String raceURI1 = "resources/races/thatOneRace.cfg";
+		String playerName1 = "Knexer";
+		String playerAddress1 = "127.0.0.1";
+		ArrayList<String> raceURIs = new ArrayList<String>();
+		raceURIs.add(raceURI1);
+		ArrayList<String> playerNames = new ArrayList<String>();
+		playerNames.add(playerName1);
+		ArrayList<String> playerAddresses = new ArrayList<String>();
+		playerAddresses.add(playerAddress1);
+
+		Game toStart = new Game(mapDefinitionURI, numPlayers, serverAddress,
+				raceURIs, playerNames, playerAddresses);
+		toStart.initialize();
+		toStart.run();
+	}
 }
