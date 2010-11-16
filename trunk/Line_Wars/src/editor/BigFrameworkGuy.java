913a098c17ad31bb3192ad49d187f738ba5f8f54
ComS 309 <knexer@gmail.com>
2010-11-16 03:33:06 +0000

    added in an export button in the editor. It copies all the folders in "bin" (the class files) and the resources folder. The export button will only show up if you opened the editor from the project, not from an exported game. Also, I added in script files that will run the game (atm just DrWeissRunMe) if you run them.
    -Connor

--- trunk/Line_Wars/src/editor/BigFrameworkGuy.java
+++ trunk/Line_Wars/src/editor/BigFrameworkGuy.java
@@ -6,16 +6,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 
 import editor.URISelector.SelectorOptions;
 import editor.abilities.AbilityEditor;
 import editor.animations.AnimationEditor;
+import editor.animations.FileCopy;
 import editor.mapEditor.MapEditor;
 import editor.mapitems.MapItemEditor;
 import editor.tech.FunctionEditor;
@@ -91,6 +94,12 @@ public class BigFrameworkGuy
 		}
 		
 		//set up the button panel
+		JButton export = null;
+		if(new File("bin").exists()) //only allow exporting if this is not a release version
+		{
+			export = new JButton("Export");
+			export.addActionListener(new ExportButtonListener());
+		}
 		JButton newer = new JButton("New");
 		newer.addActionListener(new NewButtonListener());
 		JButton load = new JButton("Load");
@@ -100,6 +109,8 @@ public class BigFrameworkGuy
 		JButton saveAs = new JButton("Save as");
 		saveAs.addActionListener(new SaveAsButtonListener());
 		JPanel buttonPanel = new JPanel();
+		if(export != null)
+			buttonPanel.add(export);
 		buttonPanel.add(newer);
 		buttonPanel.add(load);
 		buttonPanel.add(save);
@@ -360,6 +371,168 @@ public class BigFrameworkGuy
 			new SaveButtonListener().save(ce, path);
 		}
 		
+	}
+	
+	public class ExportButtonListener implements ActionListener {
+		
+		//variables to remember errors encountered
+		private ArrayList<String> errors = new ArrayList<String>();
+		
+		//variables for displaying the progress
+		private JFrame progressFrame;
+		private JTextArea work;
+		private JProgressBar bar;
+		private double size;
+		private double current;
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JFileChooser fc = new JFileChooser();
+			fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			if(fc.showOpenDialog(BigFrameworkGuy.this.frame) == JFileChooser.APPROVE_OPTION)
+			{
+				File dir = fc.getSelectedFile();
+				export(dir);
+			}
+			
+		}
+		
+		public void export(File dir)
+		{
+			//set up the file filters
+			FileFilter classesOnly = new FileFilter() {
+				public boolean accept(File f) {
+					return f.isDirectory() || f.getName().endsWith(".class");
+				}
+				public String getDescription() {
+					return ".class files";
+				}
+			};
+			FileFilter nonHiddenFiles = new FileFilter() {
+				public boolean accept(File f) {
+					return f.getName().charAt(0) != '.'; //. denotes a hidden file
+				}
+				public String getDescription() {
+					return "non-hidden files";
+				}
+			};
+			
+			//now calculate the size
+			size = 0;
+			size += copyFile(dir, new File("resources"), nonHiddenFiles, true);
+			for(File f : new File("bin").listFiles())
+				size += copyFile(dir, f, classesOnly, true);
+			
+			//set up the progress frame
+			bar = new JProgressBar();
+			bar.setMaximum((int) (size/1000.0));
+			bar.setValue(500);
+			bar.setPreferredSize(new Dimension(100, 20));
+			work = new JTextArea();
+			JScrollPane workScroller = new JScrollPane(work);
+			workScroller.setPreferredSize(new Dimension(200, 200));
+			JPanel progressPanel = new JPanel();
+			progressPanel.setLayout(new BoxLayout(progressPanel, BoxLayout.Y_AXIS));
+			progressPanel.add(workScroller);
+			progressPanel.add(bar);
+			
+			progressFrame = new JFrame("Exporting");
+			progressFrame.setContentPane(progressPanel);
+			progressFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+			progressFrame.pack();
+			progressFrame.setVisible(true);
+			
+			//now start copying
+			errors.clear();
+			current = 0;
+			copyFile(dir, new File("resources"), nonHiddenFiles, false);
+			for(File f : new File("bin").listFiles())
+				copyFile(dir, f, classesOnly, false);
+			
+			progressFrame.dispose();
+			
+			//display error dialog if there were errors
+			if(!errors.isEmpty())
+			{
+				String error = "There were errors copying the following files:\n";
+				for(String s : errors)
+					error += s + "\n";
+				JOptionPane.showMessageDialog(frame,
+					    error,
+					    "Error",
+					    JOptionPane.ERROR_MESSAGE);
+			}
+			else
+				JOptionPane.showMessageDialog(frame,
+					    "Export successfull.", 
+					    "Success",
+					    JOptionPane.DEFAULT_OPTION);
+			
+			String runCommand = "java linewars.init.DrWeissRunMe";
+			String[] runScripts = new String[]{"Windows_Run.bat", "Linux_Mac_Run.sh"};
+			for(String script : runScripts)
+			{
+				try {
+					FileWriter fw = new FileWriter(new File(dir, script));
+					fw.write(runCommand);
+					fw.flush();
+					fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		private double copyFile(File dir, File file, FileFilter filter, boolean simulate)
+		{
+			//don't copy files the filter won't allow
+			if(!filter.accept(file))
+				return 0;
+			
+			File newFile = new File(dir, file.getName());
+			
+			//if we're copying a directory
+			if(file.isDirectory())
+			{
+				if(!newFile.exists() || !newFile.isDirectory())
+				{
+					newFile.delete();
+					newFile.mkdir();
+				}
+				
+				double sum = 0;
+				
+				//copy all the contained files
+				for(File f : file.listFiles())
+					sum += copyFile(newFile, f, filter, simulate);
+				return sum;
+			}
+			else //just a single file
+			{
+				if(!newFile.exists() || newFile.isDirectory())
+					newFile.delete();
+				
+				if(!simulate)
+				{
+					double progress = ((int)(current/size*1000.0))/10.0;
+					work.setText(work.getText() + "\n" + progress + "%: " + file.getAbsolutePath());
+					System.out.println(progress + "%: " + file.getAbsolutePath());
+					work.validate();
+					work.updateUI();
+					try {
+						FileCopy.copy(file.getAbsolutePath(), newFile.getAbsolutePath());
+					} catch (IOException e) {
+						errors.add(file.getAbsolutePath());
+					}
+					current += file.length();
+					bar.setValue((int) (current/size*1000.0));
+					bar.validate();
+					bar.updateUI();
+				}
+				
+				return file.length();
+			}
+		}
 	}
 	
 	
