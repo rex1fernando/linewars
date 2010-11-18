ddbcb184466cf5e0b18e8864769c3ce4c57a8532
ComS 309 <knexer@gmail.com>
2010-11-18 02:16:09 +0000

    Finished Transformation Tests

--- trunk/Line_Wars/src/linewars/test/Ezra/TransformationTests.java
+++ trunk/Line_Wars/src/linewars/test/Ezra/TransformationTests.java
@@ -1,6 +1,8 @@
 package linewars.test.Ezra;
 
 import static org.junit.Assert.*;
+import linewars.gamestate.Position;
+import linewars.gamestate.Transformation;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,35 +10,36 @@ import org.junit.Test;
 
 public class TransformationTests {
 
+	Transformation t1, t2, t3;
+	
 	@Before
 	public void setUp()
 	{
-		//TODO:
-		//test both ctors
+		t1 = new Transformation(new Position (5.5, 5), 55);
+		t3 = new Transformation(new Position (5.5, 5), 55);
+		t2 = new Transformation (null, -5);
 	}
 	
 	@Test
 	public void testGetPosition()
 	{
-		//TODO:
+		assertEquals(t1.getPosition(), new Position (5.5, 5));
+		assertEquals(t2.getPosition(), null);
 	}
 
 	@Test
 	public void testGetRotation()
 	{
-		//TODO:
+		assertEquals(t1.getRotation(), 55, 0.0);
+		assertEquals(t2.getRotation(), -5, 0.0);
 	}
 
 	@Test
 	public void testEquals()
 	{
-		//TODO:
+		assertEquals (t1, t3);
+		assertEquals(false, t2.equals(t3));
+		assertEquals(true, t3.equals(t1));
 	}
 	
-	@Test
-	public void testToString()
-	{
-		//TODO:
-	}
-
 }
