0c516d114b4892ff641e77eb0bf7492665da777d
ComS 309 <knexer@gmail.com>
2010-11-18 02:04:24 +0000

    finished Position tests.

--- trunk/Line_Wars/src/linewars/test/Ezra/PositionTests.java
+++ trunk/Line_Wars/src/linewars/test/Ezra/PositionTests.java
@@ -15,7 +15,7 @@ import org.junit.Test;
 public class PositionTests 
 {
 
-	private Position p1, p2, p3, p4, p5, p6;
+	private Position p1, p2, p3, p4, p5, p6, p7;
 	
 	@Before
 	public void setUp() 
@@ -26,6 +26,7 @@ public class PositionTests
 		p4 = new Position ("5a66");
 		p5 = new Position (20, 0);
 		p6 = new Position (66.7, 5);
+		p7 = new Position (3, 4);
 		//TODO:
 		//test third ctor
 	}
@@ -42,9 +43,6 @@ public class PositionTests
 		assertEquals(p3.getY(), 5, 0.0);
 		assertEquals(p4.getY(), 66 , 0.0);
 	}
-	
-	
-
 	
 	@Test
 	public void testAdd()
@@ -123,55 +121,60 @@ public class PositionTests
 	@Test
 	public void testNormalize()
 	{
-		//TODO:
+		assertEquals(p1.normalize().length(), 1, 0.01);
+		assertEquals(p1.normalize(), p1.normalize());
+		assertEquals(p1.getX(), -55, 0);
+		
 	}
 	
 	@Test
 	public void testOrthogonal()
 	{
-		//TODO:
+		//simple method, just needs to compute (y, -x)
+		assertEquals(p1.orthogonal().getX(), p1.getY(), 0.0);
+		assertEquals(p1.orthogonal().getY(), -p1.getX(), 0.0);
+		assertEquals(p2.orthogonal().getX(), p2.getY(), 0.0);
+		assertEquals(p2.orthogonal().getY(), -p2.getX(), 0.0);
+		assertEquals(p1.dot(p1.orthogonal()), 0, 0.00000000000000000);
+		assertEquals(p2.dot(p2.orthogonal()), 0, 0.00000000000000000);
+		//insure that p1 remains unchanged
+		assertEquals(p1.getX(), -55, 0);
+		assertEquals(p1.getY(), 5.7, 0.0);
 	}
 	
 	@Test
 	public void testDot()
 	{
-		//TODO:
+		assertEquals(p1.dot(p2), (p1.getX() * p2.getX()) + (p1.getY()* p2.getY()) , 0.0);
+		assertEquals(p5.dot(p5), p5.getX() * p5.getX(), 0.00);
 	}
 
 	@Test
 	public void testScalarProjection()
 	{
-		//TODO:
+		assertEquals(p1.scalarProjection(p1), p1.length(), 0.0001);
+		assertEquals(p1.scalarProjection(p5), -55, 0);
+		assertEquals(p6.scalarProjection(p7), p6.dot(p7.normalize()), 0.0);
+		assertEquals(p6.getX(), 66.7, 0);
 	}
 
 	@Test
 	public void testVectorProjection()
 	{
-		//TODO:
-	}
-
-	@Test
-	public void testHashCode()
-	{
-		//TODO:
-	}
-	
-	@Test
-	public void testToString()
-	{
-		//TODO:
+		assertEquals(p1.vectorProjection(p3), p3.normalize().scale(p1.scalarProjection(p3)));
+		assertEquals(p4.vectorProjection(p5), p5.normalize().scale(p4.scalarProjection(p5)));
+		assertEquals(p5.vectorProjection(p4), p4.normalize().scale(p5.scalarProjection(p4)));
+		assertEquals(p5.vectorProjection(p5), p5);
+		assertEquals(p2.vectorProjection(p2), p2);
 	}
 
 	@Test
 	public void testLength()
 	{
-		//TODO:
-	}
-
-	@Test
-	public void testGetUnitVector()
-	{
-		//TODO:
+		assertEquals(p7.length(), 5, 0);
+		assertEquals(p3.length(), 66.88714, 0.0001);
+		assertEquals(p5.length(), 20, 0);
+		assertEquals(p1.length(), 55.29457, 0.0001);
 	}
 
 }
