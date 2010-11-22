b74b63de3de60b861cdba49fe5322be740933940
ComS 309 <knexer@gmail.com>
2010-11-22 14:39:31 +0000

    The start of the node status display.
    -Ryan Tew

--- trunk/Line_Wars/src/linewars/display/panels/NodeStatusPanel.java
+++ trunk/Line_Wars/src/linewars/display/panels/NodeStatusPanel.java
@@ -1,8 +1,11 @@
 package linewars.display.panels;
 
+import javax.swing.JList;
+
 import linewars.configfilehandler.ConfigData;
 import linewars.display.Display;
 import linewars.gameLogic.GameStateProvider;
+import linewars.gamestate.Node;
 
 /**
  * Encapsulates the information needed to display Node status information.
@@ -24,7 +27,20 @@ public class NodeStatusPanel extends Panel
 	private static final int DEFAULT_WIDTH = 500;
 	private static final int DEFAULT_HEIGHT = 400;
 	
+	/**
+	 * The location of the command button panel within the command card
+	 */
+	private static final int STATUS_PANEL_X = 11;
+	private static final int STATUS_PANEL_Y = 123;
+
+	/**
+	 * The height and width of the command button panel
+	 */
+	private static final int STATUS_PANEL_WIDTH = 345;
+	private static final int STATUS_PANEL_HEIGHT = 268;
+
 	private Display display;
+	private JList nodeStatus;
 	
 	/**
 	 * Creates this Node Status display.
@@ -37,6 +53,11 @@ public class NodeStatusPanel extends Panel
 		super(stateManager, DEFAULT_WIDTH, DEFAULT_HEIGHT, anims);
 		
 		this.display = display;
+		this.nodeStatus = new JList();
+		
+		//nodeStatus.setOpaque(false);
+		
+		add(nodeStatus);
 	}
 
 	@Override
@@ -48,5 +69,14 @@ public class NodeStatusPanel extends Panel
 
 		setSize((int)(DEFAULT_WIDTH * scaleFactor), (int)(DEFAULT_HEIGHT * scaleFactor));
 		setLocation(0, getParent().getHeight() - getHeight());
+
+		// resizes the inner panel
+		nodeStatus.setLocation((int)(STATUS_PANEL_X * scaleFactor), (int)(STATUS_PANEL_Y * scaleFactor));
+		nodeStatus.setSize((int)(STATUS_PANEL_WIDTH * scaleFactor), (int)(STATUS_PANEL_HEIGHT * scaleFactor));
+	}
+	
+	public void updateNodeStatus(Node node)
+	{
+		
 	}
 }
