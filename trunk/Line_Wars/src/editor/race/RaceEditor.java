8aed209f44d1965b027d70018f8a955fa0b4cce4
Titus Klinge <tklinge@gmail.com>
2011-03-02 19:08:10 -0600

    Updated the RaceEditor so that it works with the new configurations.

--- trunk/Line_Wars/src/editor/race/RaceEditor.java
+++ trunk/Line_Wars/src/editor/race/RaceEditor.java
@@ -1,208 +1,133 @@
 package editor.race;
 
 import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import linewars.configfilehandler.ConfigData;
-import linewars.configfilehandler.ParserKeys;
+import linewars.display.panels.TechPanel;
+import linewars.gamestate.Race;
+import linewars.gamestate.mapItems.BuildingDefinition;
+import linewars.gamestate.mapItems.GateDefinition;
+import linewars.gamestate.mapItems.UnitDefinition;
+import linewars.gamestate.tech.TechGraph;
+import configuration.Configuration;
 import editor.BigFrameworkGuy;
+import editor.BigFrameworkGuy.ConfigType;
 import editor.ConfigurationEditor;
-import editor.ListURISelector;
-import editor.URISelector;
-import editor.ListURISelector.ListSelectorOptions;
-import editor.URISelector.SelectorOptions;
+import editor.GenericSelector;
+import editor.GenericSelector.CustomToString;
+import editor.GenericSelector.GenericListCallback;
+import editor.ListGenericSelector;
 
-/**
- * An editor allowing the user to define races.
- * 
- * @author Titus Klinge
- */
+
 public class RaceEditor implements ConfigurationEditor
-{
-	/**
-	 * The one editor to control them all.
-	 */
-	private BigFrameworkGuy superEditor;
-	
-	/**
-	 * The actual panel that is used by the configuration editor.
-	 */
+{	
+	private BigFrameworkGuy bfg;
 	private RacePanel racePanel;
-
-	/**
-	 * Creates a new RaceEditor object with a reference to the BigFrameworkGuy
-	 * as its parent.
-	 * 
-	 * @param bfg The parent big framework guy of the race editor.
-	 */
+	
 	public RaceEditor(BigFrameworkGuy bfg)
 	{
-		superEditor = bfg;
+		this.bfg = bfg;
 		racePanel = new RacePanel();
-		reset();	// creates a configData object and updates the GUI
-	}
-
-	@Override
-	public void setData(ConfigData cd)
-	{
-		if (isValid(cd))
-		{
-			forceSetData(cd);
-		}
-		throw new RuntimeException("The configuration file: " + cd + " is invalid.");
-	}
-
-	@Override
-	public void forceSetData(ConfigData cd)
-	{
-		updatePanel(cd);
-	}
-
-	@Override
-	public void reset()
-	{
-		updatePanel(new ConfigData());
-	}
-
-	@Override
-	public ConfigData getData()
-	{
-		return createConfigData(racePanel);
-	}
-
-	@Override
-	public boolean isValidConfig()
-	{
-		return isValid(createConfigData(racePanel));
-	}
-
-	@Override
-	public ParserKeys getType()
-	{
-		return ParserKeys.raceURI;
 	}
 	
-	/**
-	 * Checks to see if the ConfigData object contains all the required
-	 * information to save the race it corresponds to.
-	 * 
-	 * @param cd The ConfigData object to check.
-	 * @return True if the data object has all the necessary elements.
-	 */
-	private boolean isValid(ConfigData cd)
+	@Override
+	public void setData(Configuration cd) {
+		Race race = (Race) cd;
+		
+		reset();
+		
+		racePanel.nameBox.name.setText(race.getName());
+		racePanel.commandCenter.setSelectedObject(race.getCommandCenter());
+		racePanel.gate.setSelectedObject(race.getGate());
+		racePanel.allUnits.setSelectedObjects(race.getAllUnits());
+		racePanel.enabledUnits.setSelectedObjects(race.getUnlockedUnits());
+		racePanel.allBuildings.setSelectedObjects(race.getAllBuildings());
+		racePanel.enabledBuildings.setSelectedObjects(race.getUnlockedBuildings());
+		racePanel.tech.setAllTechGraphs(race.getAllTechGraphs());
+		racePanel.tech.setEnabledTechGraphs(race.getUnlockedTechGraphs());
+	}
+
+	@Override
+	public Configuration instantiateNewConfiguration() {
+		reset();
+		return new Race();
+	}
+	
+	private void reset()
 	{
-		try
-		{
-			if (cd.getString(ParserKeys.gateURI) == null
-			 || cd.getString(ParserKeys.name) == null
-			 || cd.getString(ParserKeys.commandCenterURI) == null
-			 || cd.getStringList(ParserKeys.unitURI).size() == 0
-			 || cd.getStringList(ParserKeys.buildingURI).size() == 0)
-			{
-				return false;
-			}
-		} catch (Exception e)
-		{
-			return false;
+		racePanel.nameBox.name.setText("");
+		racePanel.commandCenter.setSelectedObject(null);
+		racePanel.gate.setSelectedObject(null);
+		racePanel.allUnits.setSelectedObjects(null);
+		racePanel.enabledUnits.setSelectedObjects(null);
+		racePanel.allBuildings.setSelectedObjects(null);
+		racePanel.enabledBuildings.setSelectedObjects(null);
+		racePanel.tech.resetTechGraphs();
+	}
+
+	@Override
+	public ConfigType getData(Configuration toSet) {
+		Race race = (Race) toSet;
+		
+		race.setName(racePanel.nameBox.name.getText());
+		
+		race.setCommandCenter((BuildingDefinition) racePanel.commandCenter.getSelectedObject());
+		race.setGate((GateDefinition) racePanel.gate.getSelectedObject());
+		for (UnitDefinition def : racePanel.allUnits.getSelectedObjects()) {
+			boolean enabled = racePanel.enabledUnits.getSelectedObjects().contains(def);
+			race.addUnit(def, enabled);
+		}
+		for (BuildingDefinition def : racePanel.allBuildings.getSelectedObjects()) {
+			boolean enabled = racePanel.enabledBuildings.getSelectedObjects().contains(def);
+			race.addBuilding(def, enabled);
 		}
 		
-		return true;
-	}
-	
-	/**
-	 * Populates the GUI with all the information stored within the ConfigData
-	 * object.
-	 * 
-	 * @param cd The config data object to populate the gui with.
-	 */
-	private void updatePanel(ConfigData cd)
-	{
-		racePanel.nameBox.name.setText((cd.getDefinedKeys().contains(ParserKeys.name)? cd.getString(ParserKeys.name) : ""));
-		racePanel.commandCenter.setSelectedURI((cd.getDefinedKeys().contains(ParserKeys.commandCenterURI)? cd.getString(ParserKeys.commandCenterURI) : ""));
-		racePanel.gate.setSelectedURI((cd.getDefinedKeys().contains(ParserKeys.gateURI)? cd.getString(ParserKeys.gateURI) : ""));
-		racePanel.unit.setSelectedURIs((cd.getDefinedKeys().contains(ParserKeys.unitURI)? cd.getStringList(ParserKeys.unitURI).toArray(new String[0]) : new String[0]));
-		racePanel.building.setSelectedURIs((cd.getDefinedKeys().contains(ParserKeys.buildingURI)? cd.getStringList(ParserKeys.buildingURI).toArray(new String[0]) : new String[0]));
-		racePanel.tech.setSelectedURIs((cd.getDefinedKeys().contains(ParserKeys.techURI)? cd.getStringList(ParserKeys.techURI).toArray(new String[0]) : new String[0]));
-	}
-	
-	/**
-	 * Creates and returns a config data object using the data stored within the
-	 * GUI fields.
-	 * 
-	 * @param rp The panel to take the data from.
-	 * @return The config data corresponding to the data stored within the panel.
-	 */
-	private ConfigData createConfigData(RacePanel rp)
-	{
-		ConfigData data = new ConfigData();
+		List<TechGraph> allGraphs = racePanel.tech.getAllTechGraphs();
+		List<TechGraph> enabledGraphs = racePanel.tech.getEnabledTechGraphs();
+		List<Boolean> en = new ArrayList<Boolean>(allGraphs.size());
+		for (int i = 0; i < allGraphs.size(); ++i) {
+			boolean enabled = (enabledGraphs.contains(allGraphs.get(i)));
+			en.add(enabled);
+		}
+		race.setTechGraphs(allGraphs, en);
 		
-		data.set(ParserKeys.name, racePanel.nameBox.name.getText());
-		data.set(ParserKeys.commandCenterURI, racePanel.commandCenter.getSelectedURI());
-		data.set(ParserKeys.gateURI, racePanel.gate.getSelectedURI());
-		data.set(ParserKeys.buildingURI, racePanel.building.getSelectedURIs());
-		data.set(ParserKeys.techURI, racePanel.tech.getSelectedURIs());
-		data.set(ParserKeys.unitURI, racePanel.unit.getSelectedURIs());
-		
-		return data;
+		return ConfigType.race;
 	}
 
 	@Override
-	public JPanel getPanel()
-	{
+	public List<ConfigType> getAllLoadableTypes() {
+		List<ConfigType> list = new ArrayList<ConfigType>();
+		list.add(ConfigType.race);
+		return list;
+	}
+
+	@Override
+	public JPanel getPanel() {
 		return racePanel;
 	}
-
-	/**
-	 * The panel that contains all the GUI elements of the race editor.
-	 */
+	
 	private class RacePanel extends JPanel
 	{
 		private static final long serialVersionUID = -4411534509382555738L;
 
-		/**
-		 * The number of units of space between elements in the panel.
-		 */
 		private static final int SPACING = 3;
-		
-		/**
-		 * A label and text field combo for typing in the name of the race.
-		 */
 		private NameBox nameBox;
+		private GenericSelector<BuildingDefinition> commandCenter;
+		private GenericSelector<GateDefinition> gate;
+		private ListGenericSelector<UnitDefinition> allUnits;
+		private ListGenericSelector<UnitDefinition> enabledUnits;
+		private ListGenericSelector<BuildingDefinition> allBuildings;
+		private ListGenericSelector<BuildingDefinition> enabledBuildings;
 		
-		/**
-		 * The selector for the command center.
-		 */
-		private URISelector commandCenter;
+		private TechPanel tech;
 		
-		/**
-		 * The selector for the gate building.
-		 */
-		private URISelector gate;
-		
-		/**
-		 * The selector for the race's units.
-		 */
-		private ListURISelector unit;
-		
-		/**
-		 * The selector for the buildings in the race.
-		 */
-		private ListURISelector building;
-		
-		/**
-		 * The selector for the techs in the race.
-		 */
-		private ListURISelector tech;
-		
-		/**
-		 * Creates a new RacePanel that is empty.
-		 */
 		public RacePanel()
 		{
 			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -210,86 +135,73 @@ public class RaceEditor implements ConfigurationEditor
 			nameBox = new NameBox();
 			add(nameBox);
 			
-			initURISelectors();
+			initConfigSelectors();
+			tech = new TechPanel(bfg);
+			tech.setPreferredSize(new Dimension(720, 480));
+			add(tech);
 		}
 		
-		/**
-		 * Initializes all the selectors.
-		 */
-		private void initURISelectors()
+		private void initConfigSelectors()
 		{
-			commandCenter = createSelector("Command Center", superEditor.getCommandCenterURIs());
-			initURISelector(commandCenter);
-			gate = createSelector("Gate", superEditor.getGateURIs());
-			initURISelector(gate);
-			unit = createListSelector("Unit", superEditor.getUnitURIs());
-			initURISelector(unit);
-			building = createListSelector("Building", superEditor.getBuildingURIs());
-			initURISelector(building);
-			tech = createListSelector("Tech", superEditor.getTechURIs());
-			initURISelector(tech);
+			commandCenter = initGenericSelector("Command Center", ConfigType.building);
+			initConfigSelector(commandCenter);
+			gate = initGenericSelector("Gate", ConfigType.gate);
+			initConfigSelector(gate);
+			
+			allUnits = initListGenericSelector("Unit", ConfigType.unit);
+			initConfigSelector(allUnits);
+			CustomToString<UnitDefinition> unitString = new GenericSelector.ShowBFGName<UnitDefinition>();
+			GenericListCallback<UnitDefinition> unitCallback = new GenericListCallback<UnitDefinition>() {
+				public List<UnitDefinition> getSelectionList() {
+					return allUnits.getSelectedObjects();
+				}
+			};
+			enabledUnits = new ListGenericSelector<UnitDefinition>("Enabled Units", unitCallback, unitString);
+			initConfigSelector(enabledUnits);
+			
+			allBuildings = initListGenericSelector("Building", ConfigType.building);
+			initConfigSelector(allBuildings);
+			CustomToString<BuildingDefinition> buildingString = new GenericSelector.ShowBFGName<BuildingDefinition>();
+			GenericListCallback<BuildingDefinition> buildingCallback = new GenericListCallback<BuildingDefinition>() {
+				public List<BuildingDefinition> getSelectionList() {
+					return allBuildings.getSelectedObjects();
+				}
+			};
+			enabledBuildings = new ListGenericSelector<BuildingDefinition>("Enabled Buildings", buildingCallback, buildingString);
+			initConfigSelector(enabledBuildings);
 		}
 		
-		/**
-		 * Creates a new selector with the given name and allowing the selection
-		 * of the list of strings.
-		 * 
-		 * @param name The name for the selector.
-		 * @param options The options to select within the selector.
-		 * @return The newly constructed selector.
-		 */
-		private URISelector createSelector(String name, final String[] options)
+		private <T extends Configuration> GenericSelector<T> initGenericSelector(String label, ConfigType c)
 		{
-			return new URISelector(name, new SelectorOptions() {
-				public String[] getOptions() { return options; }
-				public void uriSelected(String uri) {}
-			});
-		}
-
-		/**
-		 * Similar to the method above, except that it creates a list selector.
-		 * 
-		 * @param name The name of the selector.
-		 * @param options The options to be selected.
-		 * @return The selector that was constructed.
-		 */
-		private ListURISelector createListSelector(String name, final String[] options)
-		{
-			return new ListURISelector(name, new ListSelectorOptions() {
-				public String[] getOptions() { return options; }
-				public void uriSelected(String uri) {}
-				public void uriRemoved(String uri) {}
-				public void uriHighlightChange(String[] uris) {}
-			});
+			CustomToString<T> toString = new GenericSelector.ShowBFGName<T>();
+			GenericListCallback<T> callback = new GenericSelector.SelectConfigurations<T>(bfg, c);
+			GenericSelector<T> selector = new GenericSelector<T>(label, callback, toString);
+			
+			return selector;
 		}
 		
-		/**
-		 * Adds the JPanel to the race panel and adds spacing after it.
-		 * 
-		 * @param p The panel to be added.
-		 */
-		private void initURISelector(JPanel p)
+		private <T extends Configuration> ListGenericSelector<T> initListGenericSelector(String label, ConfigType c)
+		{
+			CustomToString<T> toString = new GenericSelector.ShowBFGName<T>();
+			GenericListCallback<T> callback = new GenericSelector.SelectConfigurations<T>(bfg, c);
+			ListGenericSelector<T> selector = new ListGenericSelector<T>(label, callback, toString);
+			
+			return selector;
+		}
+		
+		private void initConfigSelector(JPanel p)
 		{
 			add(p);
 			add(Box.createVerticalStrut(SPACING));
 		}
 	}
 	
-	/**
-	 * A label and text field pair for typing in a name.
-	 */
 	private class NameBox extends JPanel
 	{
 		private static final long serialVersionUID = -6205161701141390950L;
 		
-		/**
-		 * The text field holding the name.
-		 */
 		private JTextField name;
 		
-		/**
-		 * Constructs a new name box with an empty text field.
-		 */
 		public NameBox()
 		{
 			add(new JLabel("Name"));
