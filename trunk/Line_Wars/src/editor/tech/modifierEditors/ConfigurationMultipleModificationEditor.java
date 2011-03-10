d945cb15c613c43159a8c4a356c0c80b510ab385
Taylor <knexer@gmail.com>
2011-03-09 23:29:09 -0600

    Implemented MultipleSubModificationModification.java. Began implementation of ConfigurationMultipleModificationEditor.java.

--- trunk/Line_Wars/src/editor/tech/modifierEditors/ConfigurationMultipleModificationEditor.java
+++ trunk/Line_Wars/src/editor/tech/modifierEditors/ConfigurationMultipleModificationEditor.java
@@ -1,6 +1,7 @@
 package editor.tech.modifierEditors;
 
 import java.util.List;
+import java.util.Set;
 
 import javax.swing.JPanel;
 
@@ -8,60 +9,87 @@ import linewars.gamestate.tech.ModifierConfiguration;
 
 import configuration.Configuration;
 import configuration.Property;
+import configuration.Usage;
 import editor.BigFrameworkGuy.ConfigType;
 
 public class ConfigurationMultipleModificationEditor extends NewModifierEditor {
+	
+	private Usage validUsage = Usage.CONFIGURATION;
+	private Configuration template;
+	
+	private MultipleSubModificationModification data;
+	
+	private JPanel panel;
 
 	public ConfigurationMultipleModificationEditor(Property property) {
-		// TODO Auto-generated constructor stub
+		if(property.getUsage() != validUsage){
+			//TODO do something here?
+		}
+		
+		template = (Configuration) property.getValue();
+		
+		//TODO set up that JPanel
+		//we need a list selector to select a list of names to modify (from the names defined by template)
+		//the user should be able to highlight one of the names in the list to modify that sub-modification
+		//should the list selector observe data? Or should it manipulate data? both?
 	}
 
+	//if the list selector is observing data, we just have to modify data and the list selector will keep up
+	//if the list selector is manipulating data, we should call stuff in list selector and let it modify data...
+	//either way, template won't be changing...
 	@Override
 	public void setData(Configuration cd) {
-		// TODO Auto-generated method stub
-
+		//TODO
+		//cast cd to toCopy
+		
+		//empty data
+		
+		//for each key in toCopy
+			//data.put(toCopy.get(key))
 	}
 
 	@Override
 	public Configuration instantiateNewConfiguration() {
-		// TODO Auto-generated method stub
-		return null;
+		return new MultipleSubModificationModification();
 	}
 
 	@Override
 	public ConfigType getData(Configuration toSet) {
-		// TODO Auto-generated method stub
-		return null;
+		MultipleSubModificationModification target = (MultipleSubModificationModification) toSet;
+		//empty it
+		Set<String> keySet = target.getModifiedPropertyNames();
+		for(String key : keySet){
+			target.removeSubModification(key);
+		}
+		
+		//for each key in data
+		for(String key : data.getModifiedPropertyNames()){
+			target.setSubModification(key, data.getSubModification(key));
+		}
+		return ConfigType.modification;
 	}
 
 	@Override
 	public List<ConfigType> getAllLoadableTypes() {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public JPanel getPanel() {
-		// TODO Auto-generated method stub
-		return null;
+		return panel;
 	}
 
 	@Override
 	public ModifierConfiguration getData() {
-		// TODO Auto-generated method stub
-		return null;
+		MultipleSubModificationModification ret = new MultipleSubModificationModification();
+		for(String key : data.getPropertyNames()){
+			ret.setSubModification(key, data.getSubModification(key));
+		}
+		return ret;
 	}
 
 	@Override
 	public void resetEditor() {
-		// TODO Auto-generated method stub
-		
+		throw new UnsupportedOperationException();
 	}
-
-	@Override
-	public void setToModify(Property toModify) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
--- trunk/Line_Wars/src/editor/tech/modifierEditors/MultipleSubModificationModification.java
+++ trunk/Line_Wars/src/editor/tech/modifierEditors/MultipleSubModificationModification.java
@@ -1,14 +1,61 @@
 package editor.tech.modifierEditors;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import configuration.Configuration;
 import configuration.Property;
+import configuration.Usage;
 import linewars.gamestate.tech.ModifierConfiguration;
 
 public class MultipleSubModificationModification extends ModifierConfiguration {
+	
+	/**
+	 * Change this iff you change internal state variables...
+	 */
+	private static final long serialVersionUID = 7996061891926409553L;
+	
+	
+	//modifies a configuration
+	private Usage validUsage = Usage.CONFIGURATION;
+	//contains multiple sub-modifications
+	private Map<String, ModifierConfiguration> subModifications;
 
 	@Override
 	public Property applyTo(Property toModify) {
-		// TODO Auto-generated method stub
-		return null;
+		if(toModify.getUsage() != validUsage){
+			//TODO exception?
+		}
+		
+		Configuration config = (Configuration) toModify.getValue();
+		
+		for(String key : subModifications.keySet()){
+			subModifications.get(key).applyTo(config.getPropertyForName(key));
+		}
+		
+		return toModify;
+	}
+	
+	public MultipleSubModificationModification(){
+		subModifications = new HashMap<String, ModifierConfiguration>();
 	}
 
+	public ModifierConfiguration removeSubModification(String key){
+		return subModifications.remove(key);
+	}
+	
+	public ModifierConfiguration getSubModification(String key){
+		return subModifications.get(key);
+	}
+	
+	public ModifierConfiguration setSubModification(String key, ModifierConfiguration config){
+		ModifierConfiguration ret = subModifications.get(key);
+		subModifications.put(key, config);
+		return ret;
+	}
+	
+	public Set<String> getModifiedPropertyNames(){
+		return subModifications.keySet();
+	}
 }
