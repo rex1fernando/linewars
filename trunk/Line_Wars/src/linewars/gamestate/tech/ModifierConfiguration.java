c73c6e3f4b2283a71d66aa1eb003737fe7c6851c
Taylor <knexer@gmail.com>
2011-03-17 11:56:03 -0500

    Set up the framework required for implementation of the last TODO in ConfigurationMultipleModificationEditor.java.

--- trunk/Line_Wars/src/linewars/gamestate/tech/ModifierConfiguration.java
+++ trunk/Line_Wars/src/linewars/gamestate/tech/ModifierConfiguration.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.swing.JPanel;
+
 import utility.ForceLoadPackage;
 
 import configuration.Configuration;
@@ -16,25 +18,48 @@ public abstract class ModifierConfiguration extends Configuration {
 	 * 
 	 */
 	private static final long serialVersionUID = 4468407517465792016L;
-	private static HashMap<Usage, List<Class<? extends ModifierConfiguration>>> validModifiersForUsage;
+	private static HashMap<Usage, List<ModifierMetaData>> validModifiersForUsage;
 	
 	static{
 		ForceLoadPackage.forceLoadClassesInPackage(ModifierConfiguration.class.getPackage());
 	}
 	
-	public static void addModifierForUsage(Usage key, Class<? extends ModifierConfiguration> value){
-		if(validModifiersForUsage == null){
-			validModifiersForUsage = new HashMap<Usage, List<Class<? extends ModifierConfiguration>>>();
-		}
-		if(validModifiersForUsage.get(key) == null){
-			validModifiersForUsage.put(key, new ArrayList<Class<? extends ModifierConfiguration>>());
-		}
-		validModifiersForUsage.get(key).add(value);
+	public static Class<? extends ModifierConfiguration> promptUserToSelectModificationType(JPanel location, List<ModifierMetaData> validModifications){
+		
 	}
 	
-	public static List<Class<? extends ModifierConfiguration>> getModifiersForUsage(Usage key){
+	public static void addModifierForUsage(Usage key, Class<? extends ModifierConfiguration> modifierType, String description){
+		if(validModifiersForUsage == null){
+			validModifiersForUsage = new HashMap<Usage, List<ModifierMetaData>>();
+		}
+		if(validModifiersForUsage.get(key) == null){
+			validModifiersForUsage.put(key, new ArrayList<ModifierMetaData>());
+		}
+		ModifierMetaData toAdd = new ModifierMetaData(modifierType, description);
+		validModifiersForUsage.get(key).add(toAdd);
+	}
+	
+	public static List<ModifierMetaData> getModifiersForUsage(Usage key){
 		return validModifiersForUsage.get(key);
 	}
 
 	public abstract Property applyTo(Property toModify);
+	
+	public static class ModifierMetaData{
+		private final Class<? extends ModifierConfiguration> modifier;
+		private final String description;
+		
+		public ModifierMetaData(Class<? extends ModifierConfiguration> modifier, String description){
+			this.modifier = modifier;
+			this.description = description;
+		}
+		
+		public Class<? extends ModifierConfiguration> getModifier() {
+			return modifier;
+		}
+		
+		public String getDescription() {
+			return description;
+		}
+	}
 }
