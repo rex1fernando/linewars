80093b8d14618cc640b37ab27d73a9ac472c034c
Ryan Tew <rmtew2@gmail.com>
2011-03-19 13:58:58 -0500

    merged the classpath.

--- trunk/Line_Wars/src/linewars/test/CompressionTest.java
+++ trunk/Line_Wars/src/linewars/test/CompressionTest.java
@@ -1,6 +1,8 @@
 package linewars.test;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.Raster;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -17,30 +19,52 @@ public class CompressionTest
 	 */
 	public static void main(String[] args) throws IOException
 	{
-		FileInputStream file = new FileInputStream(new File(System.getProperty("user.dir") + "\\resources\\images\\Desert.png"));
-		byte[] buffer = new byte [1024];
-		int length = 0;
-		int read = 0;
-		while(read >= 0)
-		{
-			read = file.read(buffer);
-			length += read;
-		}
+		File file = new File(System.getProperty("user.dir") + "\\resources\\images\\Desert.png");
+		FileInputStream fis = new FileInputStream(file);
+		long length = file.length();
 		
 		System.out.println("file length: " + length);
 		
-		byte[] imageBytes = new byte[length];
-		file.read(imageBytes);
+		byte[] imageBytes = new byte[(int)length];
+		fis.read(imageBytes);
 		
 		ByteArrayInputStream inStream = new ByteArrayInputStream(imageBytes);
 		BufferedImage original = ImageIO.read(inStream);
 		
+		Raster data = original.getData();
+		int dataType = data.getDataBuffer().getDataType();
+		int bytes;
+		switch(dataType)
+		{
+		case DataBuffer.TYPE_BYTE:
+			bytes = Byte.SIZE / 8;
+			break;
+		case DataBuffer.TYPE_DOUBLE:
+			bytes = Double.SIZE / 8;
+			break;
+		case DataBuffer.TYPE_FLOAT:
+			bytes = Float.SIZE / 8;
+			break;
+		case DataBuffer.TYPE_INT:
+			bytes = Integer.SIZE / 8;
+			break;
+		case DataBuffer.TYPE_SHORT:
+			bytes = Short.SIZE / 8;
+			break;
+		case DataBuffer.TYPE_USHORT:
+			bytes = Short.SIZE / 8;
+			break;
+		case DataBuffer.TYPE_UNDEFINED:
+			bytes = 0;
+			break;
+		default:
+			bytes = 0;
+		}
+		
+		int channels = data.getNumBands();
 
-		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-		ImageIO.write(original, "png", outStream);
-		int bytelen = outStream.toByteArray().length;
-
-		System.out.println("byte length: " + bytelen);
+		int pixels = data.getWidth() * data.getHeight();
+		System.out.println("byte length: " + pixels * channels * bytes);
 		
 		
 //		Deflater compresser = new Deflater();
