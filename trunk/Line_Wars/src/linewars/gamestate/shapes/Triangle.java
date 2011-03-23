f29f8de96c1ee452aef8c5489863cda0c6be914b
Taylor <knexer@gmail.com>
2011-03-23 16:51:33 -0500

    Temporary commit. Working on Triangle.java.

--- trunk/Line_Wars/src/linewars/gamestate/shapes/Triangle.java
+++ trunk/Line_Wars/src/linewars/gamestate/shapes/Triangle.java
@@ -61,6 +61,7 @@ public strictfp class Triangle extends Shape {
 		//figure out which edge is longest, it will be one of the two long edges of the rectangle
 		Position longestEdge = new Position(0, 0);
 		int excludedCorner = 0;
+		ArrayList<Position> longestEdgeCorners = null;
 		for(int i = 0; i < 3; i++){
 			ArrayList<Position> includedCorners = new ArrayList<Position>();
 			for(int j = 0; j < 3; j++){
@@ -72,21 +73,23 @@ public strictfp class Triangle extends Shape {
 			if(currentEdge.length() > longestEdge.length()){
 				longestEdge = currentEdge;
 				excludedCorner = i;
+				longestEdgeCorners = includedCorners;
 			}
 		}
 		//that is the width of the rectangle
 		//compute height
-			//projection onto ortho of long side of the left-out corner minus one of the endpoints of the long side
-			Position heightVector = corners[excludedCorner].subtract(excludedCorner == 0 ? corners[1] : corners[0]).vectorProjection(longestEdge.orthogonal());
+		//projection onto ortho of long side of the left-out corner minus one of the endpoints of the long side
+		Position heightVector = corners[excludedCorner].subtract(longestEdgeCorners.get(0)).vectorProjection(longestEdge.orthogonal());
 		//compute center
+		longestEdgeCorners.add(longestEdgeCorners.get(0).add(heightVector));
+		longestEdgeCorners.add(longestEdgeCorners.get(1).add(heightVector));
+		Position center = new Position(0, 0);
+		for(int i = 0; i < 4; i++){
+			center.add(longestEdgeCorners.get(i));
+		}
 			//longestedge / 2 + height / 2
 		//compute rotation
 			//trig with the longest edge vector
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private Position[] getEdges() {
 		// TODO Auto-generated method stub
 		return null;
 	}
