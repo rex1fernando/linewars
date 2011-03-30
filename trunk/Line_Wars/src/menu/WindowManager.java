b866a3fe52f94d27ecbe26fee329b7e11d63f892
Titus Klinge <tklinge@gmail.com>
2011-03-30 15:39:06 -0500

    Added some of the artwork to the UI and began rearranging some of the files in the menu package.

--- trunk/Line_Wars/src/menu/WindowManager.java
+++ trunk/Line_Wars/src/menu/WindowManager.java
@@ -2,13 +2,18 @@ package menu;
 
 import java.awt.Dimension;
 import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import menu.creategame.CreateGamePanel;
+import menu.ContentProvider.MenuImage;
+import menu.panels.CreateGamePanel;
+import menu.panels.TitlePanel;
 
 public class WindowManager extends JFrame
 {	
@@ -22,6 +27,7 @@ public class WindowManager extends JFrame
 	public WindowManager()
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setUndecorated(true);
 		
 		titleMenu = new TitlePanel(this);
 		createMenu = new CreateGamePanel(this);
@@ -33,7 +39,10 @@ public class WindowManager extends JFrame
 	public void showWindow()
 	{
 		pack();
+		setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		
 		setVisible(true);
+		//setExtendedState(JFrame.MAXIMIZED_BOTH);
 	}
 	
 	public Dimension getPanelSize()
@@ -105,18 +114,55 @@ public class WindowManager extends JFrame
 	{
 		private static final long serialVersionUID = 2587119152843642036L;
 		private JPanel panel;
+		private MenuImage image;
 		
 		public InnerPanel(JPanel p)
 		{
+			setBackgroundImage(p);
 			panel = p;
 			add(panel);
 		}
 		
 		public void switchPanel(JPanel newPanel)
 		{
+			setBackgroundImage(newPanel);
+			
 			remove(panel);
 			panel = newPanel;
 			add(panel);
+			setBackgroundImage(newPanel);
+		}
+		
+		private void setBackgroundImage(JPanel p)
+		{
+			if (p == titleMenu) image = MenuImage.titleBackground;
+		}
+		
+		@Override
+		public void paintComponent(Graphics g)
+		{
+			Image img = ContentProvider.getImageResource(image);
+//			Image flt = null;
+//			try {
+//				flt = ImageIO.read(new File("resources/ui/OverBar1.png"));
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			
+			int sx1 = 0, sy1 = 0, sx2 = img.getWidth(null), sy2 = img.getHeight(null);
+			double this_ratio = getWidth() / (double) getHeight();
+			double img_ratio = sx2 / (double) sy2;
+			if (this_ratio < img_ratio) {
+				sx1 = (int) ((img.getWidth(null) - img.getHeight(null) * this_ratio) / 2);
+				sx2 = img.getWidth(null) - sx1;
+			} else {
+				sy1 = (int) ((img.getHeight(null) - img.getWidth(null) / this_ratio) / 2);
+				sy2 = img.getHeight(null) - sy1;
+			}
+			
+			g.drawImage(img, 0, 0, getWidth(), getHeight(), sx1, sy1, sx2, sy2, null);
+//			g.drawImage(flt, 0, 0, getWidth(), getHeight(), 0, 0, flt.getWidth(null), flt.getHeight(null), null);
 		}
 	}
 }
