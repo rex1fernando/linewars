046e61f9cfbb00388a4d005247638c2d2a700043
Taylor <knexer@gmail.com>
2011-04-11 23:53:35 -0500

    Implemented more of the Mech.  It now has correct artwork, lasers work correctly, dual lasers work correctly. TODO: test the Mech with the default plasma turret, test the Mech with the anti-projectile projectile turret, implement with upgrades and everything.

--- trunk/Line_Wars/src/linewars/gamestate/mapItems/strategies/turret/MechTurretStrategyConfiguration.java
+++ trunk/Line_Wars/src/linewars/gamestate/mapItems/strategies/turret/MechTurretStrategyConfiguration.java
@@ -3,6 +3,7 @@ package linewars.gamestate.mapItems.strategies.turret;
 import linewars.gamestate.Position;
 import linewars.gamestate.Transformation;
 import linewars.gamestate.mapItems.MapItem;
+import linewars.gamestate.mapItems.Projectile;
 import linewars.gamestate.mapItems.ProjectileDefinition;
 import linewars.gamestate.mapItems.Turret;
 import linewars.gamestate.mapItems.Unit;
@@ -30,27 +31,37 @@ public class MechTurretStrategyConfiguration extends TurretStrategyConfiguration
 	private static final Usage rangeUsage = Usage.NUMERIC_FLOATING_POINT;
 	private static final EditorUsage rangeEditorUsage = EditorUsage.PositiveReal;
 	private static final String rangeDescription = "The maximum distance that this Turret can consider targets.";
-	private static final EditorProperty rangeProperty = new EditorProperty(rangeUsage, null, rangeEditorUsage, rangeDescription);
+	private EditorProperty rangeProperty = new EditorProperty(rangeUsage, null, rangeEditorUsage, rangeDescription);
 	
 	//the minimum distance that it would like to be at from its target
 	private static final String minRangeName = "minimumRange";
 	private static final Usage minRangeUsage = Usage.NUMERIC_FLOATING_POINT;
 	private static final EditorUsage minRangeEditorUsage = EditorUsage.PositiveReal;
 	private static final String minRangeDescription = "The minimum distance that this Turret would like to maintain from its target. Note that it can still shoot at things closer than this.";
-	private static final EditorProperty minRangeProperty = new EditorProperty(minRangeUsage, null, minRangeEditorUsage, minRangeDescription);
+	private EditorProperty minRangeProperty = new EditorProperty(minRangeUsage, null, minRangeEditorUsage, minRangeDescription);
 	
 	//the type of projectile it shoots
 	private static final String projectileName = "projectile";
 	private static final Usage projectileUsage = Usage.CONFIGURATION;
 	private static final EditorUsage projectileEditorUsage = EditorUsage.ProjectileConfig;
 	private static final String projectileDescription = "The Configuration of the Projectile that this TurretStrategy should shoot.";
-	private static final EditorProperty projectileProperty = new EditorProperty(projectileUsage, null, projectileEditorUsage, projectileDescription);
+	private EditorProperty projectileProperty = new EditorProperty(projectileUsage, null, projectileEditorUsage, projectileDescription);
 	
-	//the proportion of the width of the Mech that this turret spawns projectiles in god this is so confusing...
+	//the proportion of the width of the Mech that this turret is offset by... ranges f
+	private static final String offsetName = "offset";
+	private static final Usage offsetUsage = Usage.NUMERIC_FLOATING_POINT;
+	private static final EditorUsage offsetEditorUsage = EditorUsage.Real;
+	private static final String offsetDescription = "A real-valued offset used to compute where the projectiles are spawned.\n" +
+			"If it is set to 0, projectiles will be spawned immediately in front of the Turret.\n" +
+			"If it is set to 1, projectiles will be spawned on the far right edge of the Turret.\n" +
+			"If it is set to -1, projectiles will be spawned on the far left edge of the Turret.";
+	private EditorProperty offsetProperty = new EditorProperty(offsetUsage, null, offsetEditorUsage, offsetDescription);
+	
 	public MechTurretStrategyConfiguration(){
 		this.setPropertyForName(rangeName, rangeProperty);
 		this.setPropertyForName(minRangeName, minRangeProperty);
 		this.setPropertyForName(projectileName, projectileProperty);
+		this.setPropertyForName(offsetName, offsetProperty);
 	}
 	
 	@Override
@@ -97,12 +108,20 @@ public class MechTurretStrategyConfiguration extends TurretStrategyConfiguration
 			Position relativeTarget = target.subtract(thisLocation);
 			double direction = relativeTarget.getAngle();
 			
-			//we need to compute an offset so that the projectile is spawned in front of the turret, not in the turret
+			//we need to compute a forward offset so that the projectile is spawned in front of the turret, not in the turret
 			double turretLength = owner.getBody().boundingRectangle().getWidth() / 2;
 			Position turretOffset = Position.getUnitVector(direction).scale(turretLength);
 			Position spawnAt = thisLocation.add(turretOffset);
 			
-			projectile.createMapItem(new Transformation(spawnAt, direction), owner.getOwner(), owner.getGameState());
+			//now we must also compute a sideways offset because this turret probably has to do the wierd rotate-y shit
+			double turretWidth = owner.getBody().boundingRectangle().getHeight() / 2;
+			double horizontalOffsetRatio = (Double) MechTurretStrategyConfiguration.this.getPropertyForName(offsetName).getValue();
+			double horizontalOffset = turretWidth * horizontalOffsetRatio;
+			turretOffset = Position.getUnitVector(direction + Math.PI / 2).scale(horizontalOffset);
+			spawnAt = spawnAt.add(turretOffset);
+			
+			Projectile spawnedProjectile = projectile.createMapItem(new Transformation(spawnAt, direction), owner.getOwner(), owner.getGameState());
+			owner.getWave().getLane().addProjectile(spawnedProjectile);
 		}
 
 		@Override
